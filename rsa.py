bb0997b0881f85c218b24c207f4ee999906952c1
Kevin Limacher <klimache@utbm.fr>
2019-02-08 11:48:55 +0100

    rsa termine

--- rsa.py
+++ rsa.py
@@ -1,3 +1,6 @@
+from math import *
+import random
+
 def exponentiationModulaire(x,k,n):
 	#convertion de l'exposant k en écriture binaire
 	listeBinaire = []
@@ -17,7 +20,7 @@ def exponentiationModulaire(x,k,n):
 	xk = 1
 	for i in range(len(listeX)):
 		xk *= listeX[i]
-
+		
 	#return l'application du modulo
 	return xk % n
 
@@ -26,7 +29,7 @@ def euclideEtendu(a,b):
 	u1, v1 = 1,0
 	u2, v2 = 0,1
 	while(r2!=0):
-		q=int(r1/r2)
+		q=r1//r2
 		rBuffer = r1
 		uBuffer = u1
 		vBuffer = v1
@@ -38,27 +41,51 @@ def euclideEtendu(a,b):
 		r2 = rBuffer - q*r2
 		u2 = uBuffer - q*u2
 		v2 = vBuffer - q*v2
+	print(r1, u1, v1)
 	return [r1,u1,v1]
 
 def inverseModulaire(a, N):
 	resultEuclide = euclideEtendu(a,N)
 	return resultEuclide[1]
 
-#return c et d
+#retourne c et d
 def generationExposants(p,q):
-	i=2
-	while euclideEtendu(i,(p-1)*(q-1))[0]!=1 or i <= ((p-1)*(q-1)):
-		i+=1
-	if i==((p-1)*(q-1)):
-		print("C non trouvé")
-		return
-	else:
-		c = i
+	c = random.randint(3,(p-1)*(q-1)-1)
+	while (testDePrimalite(c)!=1 or euclideEtendu(c,(p-1)*(q-1))[0]!=1):
+		c = random.randint(3,(p-1)*(q-1)-1)	
+	d = inverseModulaire(c, (p-1)*(q-1))	
+	return [c,d]
 
-	d = inverseModulaire(c, (p-1)*(q-1))
+def testDePrimalite(n):
+	if n%2==0:
+		return 0
+	if 3<sqrt(n):
+		for i in range (3,int(sqrt(n)),+2):
+			if n%i==0:
+				return 0
+	return 1
 
-	print(c)
-	print(d)
+#retourne le message chiffré
+def chiffrement(m,n,c):
+	return exponentiationModulaire(m,c,n)
 
-generationExposants(7307,5923)
-#print(inverseModulaire(7,10*16))
+#retourne le message déchiffré
+def dechiffrement(mChiffre,n,d):
+	return exponentiationModulaire(mChiffre,d,n)
+
+def mainTest():
+	p=3
+	q=11
+	n = p*q
+	phin = (p-1)*(q-1)
+	c = 3
+	d = inverseModulaire(c,phin)
+	m = 4 #message
+	
+	print('c = ',c,' ; d = ',d)
+	mChiffre = chiffrement(m, n, c)
+	print('Message chiffré : ', mChiffre)
+	m = dechiffrement(mChiffre, n, d)
+	print('Message déchiffré : ', m)
+	
+mainTest()
